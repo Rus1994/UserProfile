39bb992efcf7cfe91121ed044ec812702968b64b
Rus1994 <mirzoew.rustam@gmail.com>
2021-03-10 21:32:07 +0400

    Отладка

--- src/com/company/Homework4.java
+++ src/com/company/Homework4.java
@@ -1,6 +1,8 @@
 package com.company;
 
-import java.util.Vector;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.*;
 
 interface Warrior
 {
@@ -45,7 +47,6 @@ abstract class AbsWarrior implements Warrior
     public String toString() {
         return nameClass + " №" + IDwarrior + " Отряд: " + nameSquad;
     }
-
 }
 class Viking extends AbsWarrior
 {
@@ -55,7 +56,7 @@ class Viking extends AbsWarrior
         counterID++;
         IDwarrior = counterID;
         nameClass = "Викинг";
-        health = 70;
+        health = 80;
         damage = 70;
     }
 }
@@ -67,51 +68,180 @@ class Knight extends  AbsWarrior
         counterID++;
         IDwarrior = counterID;
         nameClass = "Рыцарь";
-        health = 100;
-        damage = 65;
+        health = 75;
+        damage = 75;
     }
 }
 class Squad
 {
     Vector<Warrior> warriorsList;
+    String nameSquad;
+    int quanVikings;
+    int quanKnights;
+    private void createWarriorsList(int _quanVikings, int _quanKnights)
+    {
+        warriorsList = new Vector<Warrior>();
+        for(int i = 0; i < _quanVikings; i++)
+            warriorsList.add(new Viking(nameSquad));
+        for(int i = 0; i < _quanKnights; i++)
+            warriorsList.add(new Knight(nameSquad));
+    }
+    Squad(String _nameSquad, int _quanVikings, int _quanKnights)
+    {
+        nameSquad = _nameSquad;
+        createWarriorsList(_quanVikings, _quanKnights);
+    }
+    public Squad(Squad obj)
+    {
+        this.nameSquad = obj.nameSquad;
+        this.quanVikings = obj.quanVikings;
+        this.quanKnights = obj.quanKnights;
+        createWarriorsList(obj.quanVikings, obj.quanKnights);
+    }
     public Warrior getRandomWarrior(){
-        int numWar = (int) Math.random()*warriorsList.size();
-        return warriorsList.get(numWar);
+        Warrior war;
+        do {
+            int numWar = (int) Math.random() * warriorsList.size();
+            war = warriorsList.get(numWar);
+        }while(!war.isAlive());
+        return war;
     }
     public boolean hasAliveWarriors(){
-        return warriorsList.size() > 0;
+        for (Warrior war : warriorsList) {
+            if(war.isAlive())
+                return true;
+        }
+        return false;
     }
-    String nameSquad;
 
     @Override
     public String toString() {
-        return "Отряд: " + nameSquad + "\n" + "Осталось бойцов: " + warriorsList.size();
+        return "Отряд: " + nameSquad + "\n";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Squad(this);
     }
 }
 class DateHelper
 {
-    public String getFormattedStartDate(){
-        return "";
+    Date startTime;
+    Date currentTime;
+    GregorianCalendar startCal;
+    GregorianCalendar currentCal;
+    DateHelper()
+    {
+        startCal = new GregorianCalendar();
+        startCal.add(Calendar.YEAR, -1500);
+        currentCal = (GregorianCalendar) startCal.clone();
     }
-    public void skipTime(){}
+    public String getFormattedStartDate(){
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd-MMM-y");
+        return formatter.format(startCal.getTime());
+    }
+    public void skipTime(){
+        currentCal.add(Calendar.MINUTE, 45);
+    }
     public String getFormattedDiff()
     {
-        return "";
+        currentTime = currentCal.getTime();
+        long diffTime = currentCal.getTime().getTime() - startCal.getTime().getTime();
+        System.out.println(diffTime);
+        Date diff = new Date(diffTime);
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(diff);
+        int y = currentCal.get(Calendar.YEAR) - startCal.get(Calendar.YEAR);
+        int m = currentCal.get(Calendar.MONTH) - startCal.get(Calendar.MONTH);
+        int d = currentCal.get(Calendar.DAY_OF_MONTH) - startCal.get(Calendar.DAY_OF_MONTH);
+        int h = currentCal.get(Calendar.HOUR_OF_DAY) - startCal.get(Calendar.HOUR_OF_DAY);
+        int min = currentCal.get(Calendar.MINUTE) - startCal.get(Calendar.MINUTE);
+        return h + ":" + min + "  " + d + "-" + m + "-" + y;
     }
 }
 class Battle
 {
+    Scanner scan;
+    Squad squad1;
+    Squad squad2;
+    int countWar;
+    Battle()
+    {
+        scan = new Scanner(System.in);
+    }
+    private Squad createSquad()
+    {
+        System.out.print("Введите название отряда: ");
+        String name = scan.nextLine();
+        int countVik = 0;
+        do {
+            if((countVik > countWar) || (countVik < 0))
+                System.out.println("Количество Викингов не может быть меньше 0 или превышать бойцов в отряде");
 
+            System.out.print("Введите количество Викингов в отряде1, остальные будут Рыцари: ");
+            countVik = scan.nextInt();
+            scan.nextLine();
+        }while((countVik > countWar) || (countVik < 0));
+        return new Squad(name, countVik, countWar-countVik);
+    }
+    public void createSquads()
+    {
+        countWar = 1;
+        do {
+            if(countWar < 1)
+                System.out.println("Количество бойцов не может быть меньше 1");
+
+            System.out.print("Введите количество бойцов в отряде: ");
+            countWar = scan.nextInt();
+            scan.nextLine();
+        }while(countWar < 1);
+
+        System.out.println("Создание первого отряда");
+        squad1 = createSquad();
+        System.out.println("Создание второго отряда");
+        squad2 = createSquad();
+    }
+    public void fight()
+    {
+        boolean attackFirst = true;
+        boolean isFinish = false;
+        AbsWarrior war1, war2;
+        createSquads();
+        DateHelper dataManag = new DateHelper();
+        System.out.println(dataManag.getFormattedStartDate());
+        while(!isFinish)
+        {
+            war1 = (AbsWarrior) squad1.getRandomWarrior();
+            war2 = (AbsWarrior) squad2.getRandomWarrior();
+            if(attackFirst)
+            {
+                war2.takeDamage(war1.attack());
+                attackFirst = false;
+                if(!squad2.hasAliveWarriors())
+                    isFinish = true;
+                System.out.println(war1.toString() + " атаковал " + war2.toString());
+            }
+            else
+            {
+                war1.takeDamage(war2.attack());
+                attackFirst = true;
+                if(!squad1.hasAliveWarriors())
+                    isFinish = true;
+                System.out.println(war2.toString() + " атаковал " + war1.toString());
+            }
+            dataManag.skipTime();
+        }
+        boolean winnerFirst = !attackFirst;
+        Squad winner = winnerFirst ? squad1 : squad2;
+        System.out.println("Победитель --> " + winner.nameSquad);
+        System.out.println("Время битвы = " + dataManag.getFormattedDiff());
+    }
 }
 
 public class Homework4 {
     static void run()
     {
-        Viking war1 = new Viking("red");
-        Knight war2 = new Knight("green");
-        Viking war3 = new Viking("green");
-        System.out.println(war1.toString());
-        System.out.println(war2.toString());
-        System.out.println(war3.toString());
+        Battle battle = new Battle();
+        battle.fight();
     }
 }
